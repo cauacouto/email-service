312848b56e7a2b17f3925fe93920e6a9d3a48e7d
cauacouto <cauacouto022@gmail.com>
2026-06-19 18:15:18 -0300

    Rename EmailService to EmailRepository for clarity in repository structure

--- src/main/java/couto/dev/email_service/Repository/EmailRepository.java
+++ src/main/java/couto/dev/email_service/Repository/EmailRepository.java
@@ -3,5 +3,5 @@ package couto.dev.email_service.Repository;
 import couto.dev.email_service.domin.EmailEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EmailService extends MongoRepository<EmailEntity,String> {
+public interface EmailRepository extends MongoRepository<EmailEntity,String> {
 }
