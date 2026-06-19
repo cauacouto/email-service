473d9c24a35f54125922d33df21300fcf7a4643c
cauacouto <cauacouto022@gmail.com>
2026-06-19 18:15:13 -0300

    Add EmailMapper to convert ReservacriadaDto to EmailEntity

---
+++ src/main/java/couto/dev/email_service/Mapper/EmailMapper.java
@@ -0,0 +1,12 @@
+package couto.dev.email_service.Mapper;
+
+import couto.dev.email_service.Dto.ReservacriadaDto;
+import couto.dev.email_service.domin.EmailEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface EmailMapper {
+
+
+    EmailEntity toEntity(ReservacriadaDto emailEntity);
+}
